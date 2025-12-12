2402cf753f509079ad08093daa96133c6d246808
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-12-12 13:51:06 +0530

    Implement reverse method for ArrayList
    Added a reverse method to reverse the elements of an ArrayList using a two-pointer approach.

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,19 +1,55 @@
 import java.util.ArrayList;
 
+// public class ArrayListDemo {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(25);
+//         arr.add(23);
+//         arr.add(7);
+//         arr.add(5);
+
+//         System.out.println(arr.get(2));
+//         arr.set(3, 16); // change a value/ update
+//         System.out.println(arr); // display a element
+//         int n = arr.size(); // find the size of arrayList
+//         for (int i = 0; i < n; i++) { // display using loop
+//             System.out.print(arr.get(i) + " ");
+//         }
+//         System.out.println();
+//         arr.add(1, 100); // add a element at specific index
+//         arr.remove(1); // remove at specific index
+//         arr.clear(); // remove element in a arrayList
+//         Collections.reverse(arr);
+//         System.out.print(arr);
+//     }
+// }
+
+// reverse a array using two pointer approach not used build in function
+
 public class ArrayListDemo {
+    public static void reverse(ArrayList<Integer> arr) {
+        int i = 0;
+        int j = arr.size() - 1;
+        while (i <= j) {
+            int temp = arr.get(i);
+            arr.set(i, arr.get(j));
+            arr.set(j, temp);
+            i++;
+            j--;
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(25);
-        arr.add(23);
-        arr.add(7);
-        arr.add(5);
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
 
-        System.out.println(arr.get(2));
-        arr.set(3, 16); // change a value/ update
-        System.out.println(arr); // display a element
-        int n = arr.size(); // find the size of arrayList
-        for (int i = 0; i < n; i++) {  // display using loop
-            System.out.print(arr.get(i) + " ");
-        }
+        System.out.print(arr);
+        System.out.println();
+        reverse(arr);
+        System.out.print(arr);
     }
 }
