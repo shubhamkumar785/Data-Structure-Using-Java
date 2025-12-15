58995e81af20007ddc418b3d07a763fe0f326a73
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-12-15 14:23:46 +0530

    Refactor BinarySearch class with multiple search methods

--- BinarySearch.java
+++ BinarySearch.java
@@ -103,3 +103,36 @@ public class BinarySearch {
         System.out.print(peakElement(arr));
     }
 }
+
+
+
+
+
+
+// search a element in sorted array in descending order
+
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, idx = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                idx = mid;
+                right = mid - 1;
+            } else if (arr[mid] > target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return idx;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 100, 91, 86, 77, 65, 52, 47, 38, 24, 16, 5 };
+        int target = 24;
+        System.out.print(search(arr, target));
+    }
+}
