8b94b6be1399af1c67a81c197f938268a14b4164
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-12-17 15:08:53 +0530

    Update peak element method

--- BinarySearch.java
+++ BinarySearch.java
@@ -85,22 +85,24 @@ public class BinarySearch {
 
 public class BinarySearch {
     public static int peakElement(int[] arr) {
-        int left = 0, right = arr.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] < arr[mid + 1]) {
+        int left = 1, right = arr.length - 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
 
         }
-        return left;
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] arr = { -1, 0, 2, 4, 6, 8, 5, 3 };
-        System.out.print(peakElement(arr));
+        System.out.println(peakElement(arr));
     }
 }
 
@@ -136,3 +138,6 @@ public class BinarySearch {
         System.out.print(search(arr, target));
     }
 }
+
+
+
