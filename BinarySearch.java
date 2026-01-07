71995922664d13d02c5dc65252be58768e120d1c
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-07 18:41:56 +0530

    Refactor BinarySearch class with multiple methods

--- BinarySearch.java
+++ BinarySearch.java
@@ -174,3 +174,28 @@ public class BinarySearch {
 
 
 // floor of square root
+
+
+public class BinarySearch {
+    public static int floorSqrt(int n) {
+        int low = 0, high = n;
+        while (low <= n) {
+            int mid = (low + high) / 2;
+            if (mid * mid == n) {
+                return mid;
+            } else if (mid * mid > n) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(floorSqrt(n));
+    }
+}
+
+
