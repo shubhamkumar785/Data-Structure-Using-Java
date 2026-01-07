f1d804e46c80d02232d76746489d806fa39b3f46
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-07 18:31:43 +0530

    Comment out power of 2 implementation

--- BitwiseOperator01.java
+++ BitwiseOperator01.java
@@ -21,24 +21,3 @@
 // }
 
 // power of 3
-
-public class BitwiseOperator01 {
-    public static boolean isPowerOFThree(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
-        return (n == 1);
-    }
-
-    public static void main(String[] args) {
-        int n = 27;
-        if (isPowerOFThree(n)) {
-            System.out.println(n + " is power of 3");
-        } else {
-            System.out.println(n + " is not power of 3");
-        }
-    }
-}
