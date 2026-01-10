9ce7cf2105017b34a2f384143736a05efee9bf2f
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-10 11:58:46 +0530

    Update BitwiseOperator01.java

--- BitwiseOperator01.java
+++ BitwiseOperator01.java
@@ -1,24 +1,24 @@
 // power of 2
-// public class BitwiseOperator01 {
-//     public static boolean isPowerOfTwo(int n) {
-//         if (n <= 0) {
-//             return false;
-//         }
-//         while (n % 2 == 0) {
-//             n = n / 2;
-//         }
-//         return (n == 1);
-//     }
+public class BitwiseOperator01 {
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        return (n == 1);
+    }
 
-//     public static void main(String[] args) {
-//         int n = 4;
-//         if (isPowerOfTwo(n)) {
-//             System.out.println(n + " is a power of 2");
-//         } else {
-//             System.out.println(n + "is not a power of 2");
-//         }
-//     }
-// }
+    public static void main(String[] args) {
+        int n = 4;
+        if (isPowerOfTwo(n)) {
+            System.out.println(n + " is a power of 2");
+        } else {
+            System.out.println(n + "is not a power of 2");
+        }
+    }
+}
 
 // power of 3
 
@@ -43,4 +43,5 @@ public class BitwiseOperator01 {
         }
     }
 }
+
 
