e73069533d89a5975d4380465a180b228f65b1ec
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-01-27 21:07:16 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -578,5 +578,22 @@
 //     }
 // }
 
+// floor squatre root
 
+public class BinarySearch {
+    public static int floorSquareRoot(int n) {
+        int root = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i * i > n) {
+                break;
+            }
+            root = i;
+        }
+        return root;
+    }
 
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(floorSquareRoot(n));
+    }
+}
