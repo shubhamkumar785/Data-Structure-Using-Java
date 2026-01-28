04c8108290235dc8aa476cd3069997587f3a934b
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-01-28 14:32:49 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -666,43 +666,223 @@
 
 // practice
 
+// public class BinarySearch {
+//     public static int findOnce(int[] arr) {
+//         int n = arr.length;
+//         if (n == 1)
+//             return arr[0];
+//         if (arr[0] != arr[1])
+//             return arr[0];
+//         if (arr[n - 1] != arr[n - 2])
+//             return arr[n - 1];
+
+//         int left = 0, right = n - 1;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
+//                 return arr[mid];
+//             }
+//             int first = mid, second = mid;
+//             if (arr[mid - 1] == arr[mid]) {
+//                 first = arr[mid - 1];
+//             } else {
+//                 second = arr[mid + 1];
+//             }
+//             int firstCount = first - left;
+//             int secondCount = right - second;
+
+//             if (firstCount % 2 == 0) {
+//                 left = second + 1;
+//             } else {
+//                 right = first - 1;
+//             }
+
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65 };
+//         System.out.println(findOnce(arr));
+//     }
+// }
+
+// first occurence
+
+// public class BinarySearch {
+//     public static int firstOcccurence(int[] arr, int target) {
+//         int low = 0, high = arr.length - 1;
+//         int first = -1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] == target) {
+//                 first = mid;
+//                 high = mid - 1;
+//             } else if (arr[mid] > target) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+
+//         }
+//         return first;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 23, 4, 5, 67, 67, 67, 89, 90 };
+//         int target = 67;
+//         System.out.println(firstOcccurence(arr, target));
+//     }
+// }
+
+// first and last occurence
+// import java.util.*;
+
+// public class BinarySearch {
+//     public static void firstAndLast(int[] arr, int target) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         int low = 0, high = arr.length - 1;
+//         int first = -1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] == target) {
+//                 first = mid;
+//                 high = mid - 1;
+//             } else if (arr[mid] > target) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         ans.add(first);
+
+//         low = 0;
+//         high = arr.length - 1;
+//         int second = -1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] == target) {
+//                 second = mid;
+//                 low = mid + 1;
+//             } else if (arr[mid] > target) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         ans.add(second);
+
+//         for (int num : ans) {
+//             System.out.print(num + " ");
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 23, 4, 5, 67, 67, 67, 89, 90 };
+//         int target = 67;
+//         firstAndLast(arr, target);
+//     }
+// }
+
+// search in descending array
+
+// public class BinarySearch {
+//     public static int search(int[] arr, int target) {
+//         int low = 0, high = arr.length - 1;
+//         int first = -1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] == target) {
+//                 first = mid;
+//                 high = mid - 1;
+//             } else if (arr[mid] > target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return first;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 90, 80, 70, 60, 60, 50, 40, 30, 10 };
+//         int target = 60;
+//         System.out.println(search(arr, target));
+//     }
+// }
+
+// find peak in a sorted array
+
+// public class BinarySearch {
+//     public static int peakElement(int[] arr) {
+//         int low = 1, high = arr.length - 2;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+//                 return arr[mid];
+//             } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { -1, 0, 1, 2, 5, 6, 7, 8, 3, 2, 1, 0, -1 };
+//         System.out.println(peakElement(arr));
+//     }
+// }
+
+// floor in a sorted array
+
+// public class BinarySearch {
+//     public static int floor(int[] arr, int x) {
+//         int low = 0, high = arr.length - 1;
+//         int idx = -1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] <= x) {
+//                 idx = arr[mid];
+//                 low = mid + 1;
+//             } else if (arr[mid] > x) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return idx;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = { 1, 2, 4, 10, 10, 12, 19 };
+//         int x = 5;
+//         System.out.println(floor(arr, x));
+//     }
+// }
+
+// square root
+
 public class BinarySearch {
-    public static int findOnce(int[] arr) {
-        int n = arr.length;
-        if (n == 1)
-            return arr[0];
-        if (arr[0] != arr[1])
-            return arr[0];
-        if (arr[n - 1] != arr[n - 2])
-            return arr[n - 1];
-
-        int left = 0, right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
-                return arr[mid];
-            }
-            int first = mid, second = mid;
-            if (arr[mid - 1] == arr[mid]) {
-                first = arr[mid - 1];
+    public static int square(int n) {
+        int low = 1, high = n;
+        int ans = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid * mid <= n) {
+                ans = mid;
+                low = mid + 1;
             } else {
-                second = arr[mid + 1];
+                high = mid - 1;
             }
-            int firstCount = first - left;
-            int secondCount = right - second;
-
-            if (firstCount % 2 == 0) {
-                left = second + 1;
-            } else {
-                right = first - 1;
-            }
-
         }
-        return -1;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65 };
-        System.out.println(findOnce(arr));
+        int n = 20;
+        System.out.println(square(n));
     }
 }
