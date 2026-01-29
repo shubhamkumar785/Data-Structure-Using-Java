ce5c9404be1331188ff76241336f50dd9839e41e
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-01-29 23:49:30 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -888,28 +888,62 @@
 
 // arrange coin
 
+// public class BinarySearch {
+//     public static int sqrt(int n) {
+//         int low = 0, high = n;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (mid * mid == n) {
+//                 return mid;
+//             } else if (mid * mid > n) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static int arrangeCoin(int n) {
+//         return (sqrt(8 * n - 1) + 1) / 2;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 13;
+//         System.out.println(arrangeCoin(n));
+//     }
+// }
+
+// search and element in sorted and rotated array
+
 public class BinarySearch {
-    public static int sqrt(int n) {
-        int low = 0, high = n;
+    public static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (mid * mid == n) {
+            if (arr[mid] == target) {
                 return mid;
-            } else if (mid * mid > n) {
-                high = mid - 1;
+            } else if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             } else {
-                low = mid + 1;
+                if (arr[mid] < target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return -1;
-    }
 
-    public static int arrangeCoin(int n) {
-        return (sqrt(8 * n - 1) + 1) / 2;
     }
 
     public static void main(String[] args) {
-        int n = 13;
-        System.out.println(arrangeCoin(n));
+        int[] arr = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+        int target = 10;
+        System.out.println(search(arr, target));
     }
 }
