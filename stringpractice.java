b5a1a7a40f09dc30cd8182599278d444b5c1cb9f
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-01-30 11:51:17 +0530

    Update stringpractice.java

--- stringpractice.java
+++ stringpractice.java
@@ -1,21 +1,42 @@
 
 // count the number of vowels in a string
 
+// public class stringpractice {
+//     public static int countVowels(String str) {
+//         int count = 0;
+//         for (int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+//             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+
+//     public static void main(String[] args) {
+//         String str = "shubhamkumar";
+//         System.out.print(countVowels(str));
+
+//     }
+// }
+
+// palindrome string used two pointer approach
+
 public class stringpractice {
-    public static int countVowels(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                count++;
+    public static boolean palindrome(String str) {
+        int low = 0, high = str.length() - 1;
+        while (low <= high) {
+            if (str.charAt(low) != str.charAt(high)) {
+                return false;
             }
+            low++;
+            high--;
         }
-        return count;
+        return true;
     }
 
     public static void main(String[] args) {
-        String str = "shubhamkumar";
-        System.out.print(countVowels(str));
-
+        String str = "madam";
+        System.out.println(palindrome(str));
     }
 }
