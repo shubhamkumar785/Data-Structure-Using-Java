961a72f205adf70bbb34329749ea0a23c736e406
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-02 22:54:32 +0530

    Add factorial function

--- recursion.java
+++ recursion.java
@@ -72,3 +72,22 @@ public class recursion1 {
 }
 
 
+// ------------------------------------------------------------------------------------------------------
+//                                    factorial number
+// ------------------------------------------------------------------------------------------------------
+
+public class recursion1 {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
+
