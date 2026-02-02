53b5c741454d9586f09b7d68dff6de9086b077fd
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-02 21:54:50 +0530

    Add method to print numbers from 1 to n

--- recursion.java
+++ recursion.java
@@ -29,3 +29,23 @@ public class recursion {
         print(10);
     }
 }
+
+
+// ------------------------------------------------------------------------------------------------------
+//                                      print 1 to n
+// ------------------------------------------------------------------------------------------------------
+
+public class recursion1 {
+    public static void print(int x, int n) {
+        if (x > n) {
+            return;
+        }
+        System.out.println(x);
+        print(x + 1, n);
+    }
+
+    public static void main(String[] args) {
+        print(1, 10);
+
+    }
+}
