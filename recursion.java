c297f122f768bb03300fef1ec845d7352e922a29
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-02 22:26:24 +0530

    Refactor recursion.java to add decreasing and increasing print

--- recursion.java
+++ recursion.java
@@ -49,3 +49,26 @@ public class recursion1 {
 
     }
 }
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+//                                    print decreasing and increasing
+// ------------------------------------------------------------------------------------------------------
+
+public class recursion1 {
+    public static void func(int n) {
+        if (n == 0)
+            return;
+        System.out.print(n + " ");
+        func(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        func(5);
+    }
+}
+
+
