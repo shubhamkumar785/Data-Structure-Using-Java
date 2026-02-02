feec94adfc659811c2b58bf1bcdf6a90abf2872c
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-02 23:56:59 +0530

    Refactor recursion methods and add power function

--- recursion.java
+++ recursion.java
@@ -133,3 +133,24 @@ public class recursion1 {
         System.out.println(sumNumber(n));
     }
 }
+
+
+
+// ------------------------------------------------------------------------------------------------------
+//                               Power of number
+// ------------------------------------------------------------------------------------------------------
+
+public class recursion1 {
+    public static int power(int n, int k) {
+        if (k == 0) {
+            return 1;
+        }
+        return n * power(n, k - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        int k = 4;
+        System.out.println(power(n, k));
+    }
+}
