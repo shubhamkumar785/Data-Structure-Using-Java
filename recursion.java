74db0e6bb37dfbb90240d7a4e3d10bfab5f041c6
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-02 21:17:55 +0530

    add recursion.java file

---
+++ recursion.java
@@ -0,0 +1,11 @@
+
+public class recursion {
+    public static void ajay() {
+        System.out.println("Priya");
+        ajay();
+    }
+
+    public static void main(String[] args) {
+        ajay();
+    }
+}
