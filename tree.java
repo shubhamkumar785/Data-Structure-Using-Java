b6466a6a5e5e03f1b7dfc40c351bcb014d5faef1
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-04 00:35:47 +0530

    Implement binary tree display functionality
    This class defines a binary tree structure and includes a method to display its nodes in a pre-order traversal.

---
+++ tree.java
@@ -0,0 +1,44 @@
+
+// ------------------------------------------------------------------------------------------
+//                                Display Binary Treee
+// ------------------------------------------------------------------------------------------
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class tree {
+    public static void display(Node root) {
+        if (root == null)
+            return;
+        System.out.print(root.val + " -> ");
+        display(root.left);
+        display(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(3);
+        Node c = new Node(5);
+        Node d = new Node(1);
+        Node e = new Node(2);
+        Node f = new Node(4);
+        Node g = new Node(7);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+
+        display(a);
+
+    }
+}
