62ae5cdea2017e52f3ebbc73b242001867b48bed
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-04 23:21:57 +0530

    sum of all nodes

--- tree.java
+++ tree.java
@@ -222,3 +222,46 @@ public class tree {
     }
 }
 
+
+
+
+
+// ------------------------------------------------------------------------------------------
+//                                sum of all node Binary Treee
+// ------------------------------------------------------------------------------------------
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree {
+
+    public static int sum(Node root) {
+        if (root == null)
+            return 0;
+        return root.data + sum(root.left) + sum(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+
+        System.out.println(sum(a));
+    }
+}
