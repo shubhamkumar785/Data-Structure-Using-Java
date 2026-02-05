65cea70d35f49594cf95fd96930a994d616a22cf
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-05 08:55:16 +0530

    level of a tree method

--- tree.java
+++ tree.java
@@ -352,3 +352,44 @@ public class tree {
     }
 }
 
+
+
+
+// ------------------------------------------------------------------------------------------
+//                                level of a binary tree
+// ------------------------------------------------------------------------------------------
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree {
+    public static int level(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(level(root.left), level(root.right));
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+
+        System.out.println(level(a));
+    }
+}
