4a627f6ab185b7ed6525265a1cf3356994c15491
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-09 22:59:25 +0530

    Refactor tree class and node structure

--- tree.java
+++ tree.java
@@ -607,3 +607,55 @@ public class tree {
 }
 
 
+
+
+
+
+// ------------------------------------------------------------------------------------------
+//                                 DFS - PostOrder
+// ------------------------------------------------------------------------------------------
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree {
+
+    public static void display(Node root) {
+        if (root == null)
+            return;
+
+        display(root.left);
+        display(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+
+        display(a);
+    }
+}
+
+
+
+
+
+
