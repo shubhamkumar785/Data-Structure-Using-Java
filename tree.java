901719793a785fbede7875321125680727ce47a1
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-09 22:22:27 +0530

    implement DFS (Inorder)

--- tree.java
+++ tree.java
@@ -517,3 +517,93 @@ public class tree {
         System.out.println(identical(a1, a2));
     }
 }
+
+
+// ------------------------------------------------------------------------------------------
+//                                 DFS - Preorder
+// ------------------------------------------------------------------------------------------
+class Node {
+   int data;
+   Node left;
+   Node right;
+
+   Node(int data) {
+       this.data = data;
+   }
+}
+
+public class tree {
+
+   public static void display(Node root) {
+       if (root == null)
+           return;
+
+       System.out.print(root.data + " ");
+       display(root.left);
+       display(root.right);
+   }
+
+   public static void main(String[] args) {
+
+       Node a = new Node(10);
+       Node b = new Node(20);
+       Node c = new Node(30);
+       Node d = new Node(40);
+       Node e = new Node(50);
+
+       a.left = b;
+       a.right = c;
+       b.left = d;
+       b.right = e;
+
+       display(a);
+   }
+}
+
+
+
+
+// ------------------------------------------------------------------------------------------
+//                                 DFS - Inorder
+// ------------------------------------------------------------------------------------------
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree {
+
+    public static void display(Node root) {
+        if (root == null)
+            return;
+
+        display(root.left);
+        System.out.print(root.data + " ");
+        display(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+
+        display(a);
+    }
+}
+
+
