212f16b851197a7c53587819416105bde624fbd0
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-09 23:03:34 +0530

    size of a binary tree

--- tree.java
+++ tree.java
@@ -659,3 +659,58 @@ public class tree {
 
 
 
+
+// ------------------------------------------------------------------------------------------
+//                                 size of binary tree
+// ------------------------------------------------------------------------------------------
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree {
+
+    public static int size(Node root) {
+    	if(root == null) {
+    		return 0;
+    	}
+    	return 1 + size(root.left) + size(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+
+        System.out.println(size(a));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
