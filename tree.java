1bb2a69359a12d80facfe08e8d3b03b4c3550e81
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-10 23:50:57 +0530

    implement symmetric tree

--- tree.java
+++ tree.java
@@ -797,3 +797,53 @@ public class tree {
 }
 
 
+
+
+
+// ------------------------------------------------------------------------------------------
+//                                 symmetric binary tree
+// ------------------------------------------------------------------------------------------
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class tree1 {
+    public static boolean isSymmetric(Node root) {
+        if (root == null)
+            return true;
+        return isMirror(root.left, root.right);
+    }
+
+    public static boolean isMirror(Node left, Node right) {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.data != right.data)
+            return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(2);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+
+        root.right.left = new Node(4);
+        root.right.right = new Node(3);
+
+        System.out.println(isSymmetric(root)); // true
+    }
+}
+
