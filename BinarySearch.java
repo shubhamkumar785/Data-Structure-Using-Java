13ce9ee6b9de98225530cea768157ed8e1fd4019
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-17 00:28:39 +0530

    add implementation to find floor in a sorted array

--- BinarySearch.java
+++ BinarySearch.java
@@ -1040,3 +1040,37 @@ public class bs {
 
 
  */
+
+
+
+/*
+
+ Floor in s sorted array
+
+*/
+
+public class bs {
+    public static int floor(int[] arr, int x) {
+        int low = 0, high = arr.length;
+        int idx = -1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > x) {
+                high = mid - 1;
+            } else if (arr[mid] <= x) {
+                idx = mid;
+                low = mid + 1;
+            }
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 8, 10, 10, 12, 19 }; // 6
+        int x = 20;
+        System.out.println(floor(arr, x));
+    }
+}
+
