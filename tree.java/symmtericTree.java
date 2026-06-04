69e3fea31c8916966e06f76078f6f6975c5cd49f
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-06-04 10:29:48 +0530

    to check symmetric tree

--- tree.java/symmtericTree.java
+++ tree.java/symmtericTree.java
@@ -10,28 +10,30 @@ class Node {
 }
 
 public class symmtericTree {
-    public static boolean isSymmetric(Node root) {
-        if (root == null) {
-            return true;
-        }
-        return isMirror(root.left, root.right);
 
-    }
-
-    public static boolean isMirror(Node left, Node right) {
-        if (left == null && right == null) {
+    public static boolean isSymmetric(Node left, Node right) {
+        if (left == null && right == null)
             return true;
-        }
-        if (left == null || right == null) {
+        if (left == null || right == null)
             return false;
-        }
-        if (left.data != right.data) {
+        if (left.data != right.data)
             return false;
-        }
-        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 
     public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        a.left = b;
+        a.right = c;
+        b.right = d;
+        c.left = e;
+
+        System.out.println(isSymmetric(a.left, a.right));
 
     }
 }
